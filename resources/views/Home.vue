80bb12608b479a0a79f258029383dba9093c1219
onim4ru <jeanlamour.en@gmail.com>
2018-02-27 23:34:06 +0100

    avancement site nakama

--- resources/views/Home.vue
+++ resources/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
-    <h1>This is an home page</h1>
+    <span class="md-title">Un open de Kendo à Poitiers</span>
+    <p class="md-body-1">
+      Le Kendo Club Poitiers organise les 07 et 8 Avril 2018 une compétition de Kendo gratuite et ouverte à tous. 
+      
+    </p>
+
   </div>
 </template>
 
--- resources/views/MainLayout.vue
+++ resources/views/MainLayout.vue
@@ -1,14 +1,125 @@
 <template>
   <div class="page-container">
-    <NavBar></NavBar>
-    <router-view></router-view>
+    <div class="navbar">
+      <md-app md-waterfall md-mode="fixed-last">
+       <md-app-toolbar class="md-large md-dense md-primary">
+          <div class="md-toolbar-row">
+            <div class="md-toolbar-section-start">
+              <md-button class="md-icon-button" @click="toggleMenu" v-if="!menuVisible">
+                <md-icon>menu</md-icon>
+              </md-button>
+
+              <span class="md-title">
+                <!-- <md-icon>face</md-icon> Nakama Cup </span> -->
+              Nakama Cup | <span class="md-subheading">Open de Kendo à Poitiers</span></span>
+            </div>
+
+          </div>
+
+          <div class="md-toolbar-row navbarFixed md-xsmall-hide md-small-hide">
+            <div class="md-primary md-xsmall-hide md-small-hide">
+              <md-button :to="{name: 'home'}" class="md-primary">Home</md-button>
+              <md-button :to="{name: 'programme'}" class="md-primary">Programme</md-button>
+              <md-button :to="{name: 'info'}">Info</md-button>
+              <md-button :to="{name: 'media'}" class="md-primary">Media</md-button>
+              <md-button :to="{name: 'contact'}">Contact</md-button>
+              <md-button :to="{name: 'matsuri'}" class="md-primary">Festival Matsuri</md-button>
+            </div>
+          </div>
+        </md-app-toolbar>
+
+        <!-- <md-app-drawer class="md-right" :md-active.sync="menuMoreVisible">
+          <md-toolbar class="md-transparent" md-elevation="0">Navigation</md-toolbar>
+
+          <md-list>
+            <md-list-item>
+              <md-icon>move_to_inbox</md-icon>
+              <span class="md-list-item-text">Inbox</span>
+            </md-list-item>
+
+            <md-list-item>
+              <md-icon>send</md-icon>
+              <span class="md-list-item-text">Sent Mail</span>
+            </md-list-item>
+          </md-list>
+        </md-app-drawer> -->
+
+        <md-app-drawer :md-active.sync="menuVisible" md-persistent="mini">
+          <md-toolbar class="md-transparent" md-elevation="0">
+            <span>Navigation</span>
+
+            <div class="md-toolbar-section-end">
+              <md-button class="md-icon-button md-dense" @click="toggleMenu">
+                <md-icon>keyboard_arrow_left</md-icon>
+              </md-button>
+            </div>
+          </md-toolbar>
+
+          <md-list>
+            <md-list-item>
+              <md-icon>assignment</md-icon>
+              <span class="md-list-item-text">Inscription</span>
+            </md-list-item>
+
+            <md-list-item>
+              <md-icon>send</md-icon>
+              <span class="md-list-item-text">Sent Mail</span>
+            </md-list-item>
+
+            <md-list-item>
+              <md-icon>delete</md-icon>
+              <span class="md-list-item-text">Trash</span>
+            </md-list-item>
+
+            <md-list-item>
+              <md-icon>error</md-icon>
+              <span class="md-list-item-text">Spam</span>
+            </md-list-item>
+          </md-list>
+        </md-app-drawer>
+         <md-app-content>
+          <router-view></router-view>
+         </md-app-content>
+      </md-app>
+    </div>
+
   </div>
 </template>
 
+
 <script>
-  import NavBar from '@components/NavBar'; // @ is an alias to /src
+  import Vue from 'vue'
+  // import NavBar from '@components/NavNar.vue'
 
   export default{
-    components: { NavBar },
+    // components: { NavBar },
+    data: function () {
+      return {
+        menuVisible: false
+      }
+    },
+
+    methods: {
+      toggleMenu(){ this.menuVisible = !this.menuVisible }
+    }
   }
 </script>
+
+<style lang="scss" scoped>
+  .md-app {
+    min-height: 98vh;
+    border: 1px solid rgba(#000, .12);
+  }
+  .md-drawer {
+    width: 230px;
+    max-width: calc(100vw - 125px);
+  }
+  .navbarFixed{
+    padding-left: 48px;
+  }
+  .md-content-main{
+    padding-left: 100px;
+  }
+</style>
+
+
